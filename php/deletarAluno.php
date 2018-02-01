33d5107443d8744da602cc9f0f51e2d285c38fe5
njr1466 <njr1466@gmail.com>
2018-02-01 16:04:20 -0300

    deleta aluno em todas as tabelas

--- php/deletarAluno.php
+++ php/deletarAluno.php
@@ -1,32 +1,34 @@
-<?php include 'VerificarSession2.php'?>
+<?php include 'VerificarSession.php'?>
 <?php
 
 	include 'Conexao.php';
 $idaluno= $_GET["idaluno"];
 $nomealuno= $_GET["nomealuno"];
+
 		try{
-		
-		
-		
-		$stmt = $conexao->prepare("delete from aluno_disciplina where aluno_idaluno = ?");
 
-		 	 	   
-		
-		$stmt -> bindParam(1,$idaluno);    
-		$stmt->execute(); 
 $stmt = $conexao->prepare("delete from aluno where idaluno = ?");
-
-		 	 	   
 		
 		$stmt -> bindParam(1,$idaluno);    
 		$stmt->execute();
+
 		
+$stmt = $conexao->prepare("delete from aluno_disciplina where aluno_idaluno = ?");
+		
+		$stmt -> bindParam(1,$idaluno);    
+		$stmt->execute();
+
+
+$stmt = $conexao->prepare("delete from aluno_turma where aluno_idaluno = ?");
+		 	 	   		
+		$stmt -> bindParam(1,$idaluno);    
+		$stmt->execute();		
+
 	$_SESSION['erromatricula'] = "<b>Aluno ".$nomealuno." Deletado com sucesso!</b>";
 		echo "<script language= 'JavaScript'>
 location.href='../mensagem.php?msg=Excluído&url=registraralunos.php'
 </script>"; 	
-			
-		
+					
 		}catch(PDOException $e){
 		echo 'ERROR: ' . $e->getMessage();
 
